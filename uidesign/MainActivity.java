fb8c08cb7a17ff4d8071f056389552c40405ecf9
Srutheeshna <76912925+Srutheeshna@users.noreply.github.com>
2022-03-25 15:00:39 +0530

    Create MainActivity.java

---
+++ uidesign/MainActivity.java
@@ -0,0 +1,19 @@
+public class MainActivity extends AppCompatActivity {
+Button button;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main);
+button=findViewById(R.id.button);
+button.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+Intent i=new Intent(getApplicationContext(),MainActivity2.class);
+startActivity(i);
+
+53
+
+}
+});
+}
+}
